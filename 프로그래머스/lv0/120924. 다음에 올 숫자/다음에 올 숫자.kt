8cab514a538877ccb15ca4da0403f2d88aaaf03e
노원진 <75117860+won-droid@users.noreply.github.com>
2023-03-30 12:47:01 +0900

    [level 0] Title: 다음에 올 숫자, Time: 13.54 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(common: IntArray): Int = if (common[1] - common[0] == common[2] - common[1]) {
+        common.last() + common[1] - common[0]
+    } else {
+        common.last() * common[1] / common[0]
+    }
+}
